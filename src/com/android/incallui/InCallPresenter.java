c0145943e3b3c7430b589b74d865a03bbb115d86
Raja Mungamuri <Raja1612.M@gmail.com>
2015-06-20 15:01:40 -0700

    Fixed incoming call handling while heads up off : CM defects.
    - Fixed the No InCallUI launch during incoming call with heads up off.
    - Fixed the FC during call interrupted by otherside party.
    - Fixed the FC during call interrupted by self.
    
    //TODO: More need to be check why theming is called while incoming call ends.
    //TODO: During the incoming call : Double tap to awake screen not working after
    once turning off the screen with power button or double tap on status bar.
    
    Change-Id: Iacb757af196fabbcb4d5bf7f0f143876950862a9

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -642,6 +642,9 @@ public class InCallPresenter implements CallList.Listener,
 
     public void cancelAccountSelection() {
         mAccountSelectionCancelled = true;
+        if(mCallList==null){
+           return; //TODO: what is exactly happening here.
+        }
         Call call = mCallList.getWaitingForAccountCall();
         if (call != null) {
             String callId = call.getId();
@@ -1363,7 +1366,7 @@ public class InCallPresenter implements CallList.Listener,
                 | Intent.FLAG_ACTIVITY_NO_USER_ACTION | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         if (newTask) {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         }
 
         intent.setClass(mContext, InCallActivity.class);
@@ -1544,7 +1547,7 @@ public class InCallPresenter implements CallList.Listener,
     public void setThemeColors() {
         // This method will set the background to default if the color is PhoneAccount.NO_COLOR.
         mThemeColors = getColorsFromCall(CallList.getInstance().getFirstCall());
-
+        //TODO: is the above method need to be called here ?
         if (mInCallActivity == null) {
             return;
         }
@@ -1565,16 +1568,17 @@ public class InCallPresenter implements CallList.Listener,
 
     private MaterialPalette getColorsFromPhoneAccountHandle(PhoneAccountHandle phoneAccountHandle) {
         int highlightColor = PhoneAccount.NO_HIGHLIGHT_COLOR;
-        if (phoneAccountHandle != null) {
-            final TelecomManager tm = getTelecomManager();
+        if (phoneAccountHandle == null || mContext == null) { //OMGFIX, serious need do this in better way.
+            return null;
+        }
+        final TelecomManager tm = getTelecomManager();
 
-            if (tm != null) {
-                final PhoneAccount account = tm.getPhoneAccount(phoneAccountHandle);
-                // For single-sim devices, there will be no selected highlight color, so the phone
-                // account will default to NO_HIGHLIGHT_COLOR.
-                if (account != null) {
-                    highlightColor = account.getHighlightColor();
-                }
+        if (tm != null) {
+            final PhoneAccount account = tm.getPhoneAccount(phoneAccountHandle);
+            // For single-sim devices, there will be no selected highlight color, so the phone
+            // account will default to NO_HIGHLIGHT_COLOR.
+            if (account != null) {
+                highlightColor = account.getHighlightColor();
             }
         }
         return new InCallUIMaterialColorMapUtils(
